cc589da54470b6ab5f4140ffb03d7d77d1329cdb
Achraf EL AFFAS <77996504+achrafelaffas@users.noreply.github.com>
2023-08-12 11:45:22 +0100

    env

---
+++ env.c
@@ -0,0 +1,27 @@
+#include "header.h"
+
+/**
+ * _getenv - get an env var
+ * @name: name of the var
+ * Return: returns a pointer to the value
+ * part of the environment variable or NULL
+ */
+
+char *_getenv(const char *name)
+{
+size_t len;
+char **env;
+
+if (name == NULL || *environ == NULL)
+return (NULL);
+
+len = _strlen(name);
+
+for (env = environ; *env != NULL; env++)
+{
+if (_strncmp(*env, name, len) == 0 && (*env)[len] == '=')
+return (*env + len + 1);
+}
+
+return (NULL);
+}
